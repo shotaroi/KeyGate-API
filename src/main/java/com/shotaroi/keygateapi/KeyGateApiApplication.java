5540b8825c835fdfe364482e7fa72e8ecc16693d
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-31 19:04:31 +0100

    Create KeyGateApiApplication.java

---
+++ src/main/java/com/shotaroi/keygateapi/KeyGateApiApplication.java
@@ -0,0 +1,13 @@
+package com.shotaroi.keygateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyGateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyGateApiApplication.class, args);
+    }
+
+}
