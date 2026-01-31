609a17aafba30cf21fb8027026e1cfc7f2fac14d
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-31 19:04:25 +0100

    Create KeyGateApiApplicationTests.java

---
+++ src/test/java/com/shotaroi/keygateapi/KeyGateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shotaroi.keygateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyGateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
